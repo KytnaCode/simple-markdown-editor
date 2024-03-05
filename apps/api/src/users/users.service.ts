9569a0e3c7ecc552d908d9737028c86c498b9cef
Kytnacode <kytnacode@gmail.com>
2024-03-04 17:17:08 -0700

    Remove unused imports

--- apps/api/src/users/users.service.ts
+++ apps/api/src/users/users.service.ts
@@ -1,13 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './user.entity';
-import {
-  DeleteResult,
-  FindOptionsWhere,
-  InsertResult,
-  Repository,
-  UpdateResult,
-} from 'typeorm';
+import { DeleteResult, InsertResult, Repository } from 'typeorm';
 import { QueryDeepPartialEntity } from 'typeorm/query-builder/QueryPartialEntity';
 
 @Injectable()
